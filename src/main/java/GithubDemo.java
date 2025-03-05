ed10b0181fb2bfe0a684cfb0b0c3b5e19fa9a1c5
Danny4life <osiki.segun@gmail.com>
2025-03-05 17:26:27 +0100

    i modified some files

---
+++ src/main/java/GithubDemo.java
@@ -0,0 +1,9 @@
+public class GithubDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println("I am leaning git / github");
+
+        System.out.println("I am modifying my code");
+    }
+}
